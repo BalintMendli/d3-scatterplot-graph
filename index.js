16044267c8587c74d6e1df411c6d5780cb9cce37
zenott <mblevel@gmail.com>
2018-09-22 11:50:58 +0200

    Add tooltips and change linting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   d3.json(
-    "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
+    'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json'
   ).then(data => {
     const points = [];
     data.forEach(element => {
@@ -11,31 +11,35 @@ document.addEventListener("DOMContentLoaded", () => {
           0,
           0,
           0,
-          element.Time.split(":")[0],
-          element.Time.split(":")[1]
+          element.Time.split(':')[0],
+          element.Time.split(':')[1]
         ),
-        element.Doping
+        element.Doping,
+        element.Name,
+        element.Nationality,
+        element.Time,
+        element.Doping,
       ]);
     });
     console.log(points);
-    d3.select("#container")
-      .append("h1")
-      .text("Doping in Professional Bicycle Racing")
-      .attr("id", "title");
-    d3.select("#container")
-      .append("h2")
+    d3.select('#container')
+      .append('h1')
+      .text('Doping in Professional Bicycle Racing')
+      .attr('id', 'title');
+    d3.select('#container')
+      .append('h2')
       .text("35 Fastest times up Alpe d'Huez")
-      .attr("id", "subtitle");
+      .attr('id', 'sub-title');
 
     const w = 900;
     const h = 600;
     const padding = 40;
 
     const svg = d3
-      .select("#container")
-      .append("svg")
-      .attr("width", w)
-      .attr("height", h);
+      .select('#container')
+      .append('svg')
+      .attr('width', w)
+      .attr('height', h);
 
     const xScale = d3
       .scaleTime()
@@ -45,40 +49,41 @@ document.addEventListener("DOMContentLoaded", () => {
     const yScale = d3
       .scaleTime()
       .domain([d3.min(points, d => d[1]), d3.max(points, d => d[1])])
-      .range([padding, h - padding]);
+      .range([padding - 20, h - padding]);
 
     svg
-      .selectAll("circle")
+      .selectAll('circle')
       .data(points)
       .enter()
-      .append("circle")
-      .attr("cx", d => xScale(d[0]))
-      .attr("cy", d => yScale(d[1]))
-      .attr("r", 5)
-      .attr("data-xvalue", d => d[0])
-      .attr("data-yvalue", d => d[1])
-      .attr("fill", "royalblue")
-      .attr("class", "point")
-      .append("title")
-      .text(d => d[1])
-      .attr("id", "tooltip");
+      .append('circle')
+      .attr('cx', d => xScale(d[0]))
+      .attr('cy', d => yScale(d[1]))
+      .attr('r', 5)
+      .attr('data-xvalue', d => d[0])
+      .attr('data-yvalue', d => d[1])
+      .attr('fill', d => (d[2] === '' ? 'orange' : 'royalblue'))
+      .attr('stroke', 'black')
+      .attr('class', 'point')
+      .append('title')
+      .text(d => `${d[3]}: ${d[4]}\nYear: ${d[0]}, Time: ${d[5]}\n\n${d[6]}`)
+      .attr('id', 'tooltip');
 
-    const xAxis = d3.axisBottom(xScale).tickFormat(d3.format(".4r"));
+    const xAxis = d3.axisBottom(xScale).tickFormat(d3.format('.4r'));
 
     svg
-      .append("g")
-      .attr("transform", `translate(0, ${h - padding})`)
+      .append('g')
+      .attr('transform', `translate(0, ${h - padding})`)
       .call(xAxis)
-      .attr("id", "x-axis");
+      .attr('id', 'x-axis');
 
     const yAxis = d3.axisLeft(yScale);
 
-    yAxis.tickFormat(d3.timeFormat("%M:%S"));
+    yAxis.tickFormat(d3.timeFormat('%M:%S'));
 
     svg
-      .append("g")
-      .attr("transform", `translate(${padding},0)`)
+      .append('g')
+      .attr('transform', `translate(${padding},0)`)
       .call(yAxis)
-      .attr("id", "y-axis");
+      .attr('id', 'y-axis');
   });
 });
